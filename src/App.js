0152925e99151cb600c00cf355a988b4955ce03d
GOGU EDUKONDAL <97788201+goguedukondal@users.noreply.github.com>
2022-10-31 16:20:27 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>The Evolution Of Airbnb's Frontend</h1>
+      <p>@spikebrehm</p>
+      <img src='https://www.bing.com/th?id=OIP.yqTY33JEGacgFl9EhcHBWQHaE8&w=306&h=204&c=8&rs=1&qlt=90&o=6&dpr=1.5&pid=3.1&rm=2'/>
+    </div>
+  );
+}
+
+export default App;
